956017a1c25028218c79b1d00fd5596ae0f1cb62
Pratapaditya Chander <63329229+VemorPhose@users.noreply.github.com>
2025-01-18 19:03:53 +0530

    Update ProblemPage.jsx, preparing for backend link (#3)

---
+++ client/src/boilerplates/boilerplate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Algorank
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Start typing here...
+        
+	}
+}
