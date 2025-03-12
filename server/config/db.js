67569badda2866cc12c3201df5847bbcef2c2300
Vemor Phose <anonygamer666@gmail.com>
2025-03-12 23:12:07 +0530

    Revert "supabase setup 2/n"
    This reverts commit d808988601cf057d69ba82ebdd6bcd502aaa97f8.

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl: process.env.NODE_ENV === 'production' ? {
     rejectUnauthorized: false
-  }
+  } : false
 });
 
 module.exports = pool;
